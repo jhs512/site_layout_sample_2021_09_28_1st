a378d703c6c7206d8d84c6019f674f78609060fc
jhs512 <jangka512@gmail.com>
2021-09-28 17:56:41 +0900

    섹션 2를 지나면 탑바의 디자인 변경

--- head.php
+++ head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,10 @@
   <title>신라호텔</title>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+  <!-- gsap -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollTrigger.min.js"></script>
 
   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
@@ -16,25 +21,29 @@
   <link rel="stylesheet" href="common.css">
   <script src="common.js" defer></script>
 
-  <link rel="stylesheet" href="<?=$pageCode?>.css">
-  <script src="<?=$pageCode?>.js" defer></script>
+  <link rel="stylesheet" href="<?= $pageCode ?>.css">
+  <script src="<?= $pageCode ?>.js" defer></script>
 </head>
-<body>
-  
-<div class="site-wrap con-min-width">
-  <header class="top-bar con-min-width bg-red-500">
-    <div class="flex justify-end pr-10">
-      <nav class="menu-box-1">
-        <ul class="flex">
-          <li><a href="#" class="block p-3"><span>내용</span></a></li>
-          <li><a href="#" class="block p-3"><span>내용</span></a></li>
-          <li><a href="#" class="block p-3"><span>내용</span></a></li>
-        </ul>
-      </nav>
-    </div>
-    <div class="con con-padd mx-auto">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores molestiae amet commodi aut! Labore eligendi veritatis non id quam! Voluptatibus tenetur ex voluptatem id autem vero rerum officia voluptates numquam.
-    </div>
-  </header>
 
-  <main>
+<body>
+
+  <div class="site-wrap con-min-width">
+    <header class="top-bar con-min-width">
+      <div class="flex justify-end pr-10">
+        <nav class="menu-box-1">
+          <ul class="flex">
+            <li><a href="#" class="block p-3"><span>내용</span></a></li>
+            <li><a href="#" class="block p-3"><span>내용</span></a></li>
+            <li><a href="#" class="block p-3"><span>내용</span></a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="con con-padd mx-auto">
+        <a href="#" class="logo">
+          <img src="https://via.placeholder.com/50/FFFFFF/000000?text=LOGO">
+          <img src="https://via.placeholder.com/50/000000/FFFFFF?text=LOGO">
+        </a>
+      </div>
+    </header>
+
+    <main>
