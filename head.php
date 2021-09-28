f770b08a38019e75cb80f7df1dc5dfdcb09d76cf
jhs512 <jangka512@gmail.com>
2021-09-28 16:40:00 +0900

    세팅

---
+++ head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>신라호텔</title>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+  <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.16/tailwind.min.css">
+
+  <link rel="stylesheet" href="common.css">
+  <script src="common.js" defer></script>
+
+  <link rel="stylesheet" href="<?=$pageCode?>.css">
+  <script src="<?=$pageCode?>.js" defer></script>
+</head>
+<body>
+  
+<nav>
+  <a href="index.ssg.php" class="logo">로고</a>
+  <a href="login.ssg.php" class="logo">로그인</a>
+</nav>
